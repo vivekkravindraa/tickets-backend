90fad7589c248b723de557bd421b7c18417bba0f
vivek <vivek.vkit@gmail.com>
2018-08-22 12:29:48 +0530

    new commit

--- models/ticket.js
+++ models/ticket.js
@@ -27,6 +27,16 @@ const ticketSchema = mongoose.Schema({
     }
 })
 
+// Do not define instance or class methods in arrow functions
+ticketSchema.statics.openTickets = function() {
+    return this.openTickets({status: 'open'});
+    // 'this' here refers to the class 'Ticket'
+}
+
+ticketSchema.statics.completedTickets = function() {
+    return this.completedTickets({status: 'completed'});
+}
+
 const Ticket = mongoose.model('Tickets', ticketSchema);
 
 module.exports = { Ticket }
